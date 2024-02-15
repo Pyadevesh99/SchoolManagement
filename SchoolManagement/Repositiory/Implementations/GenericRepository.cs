95c740ec3f077e5a770b1b8f1f2cac0fedd35c60
pyade <pyade@DESKTOP-SS0DTK1>
2024-02-15 20:46:17 +0530

    Add project files.

---
+++ SchoolManagement/Repositiory/Implementations/GenericRepository.cs
@@ -0,0 +1,77 @@
+﻿using Data.Repositiory.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Repositiory.Implementations
+{
+    public class GenericRepository<T> : IDisposable, IGenericRepositiory<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private DbSet<T> _dbSet;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>(); 
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> AddSync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteById(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAll(System.Linq.Expressions.Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperty = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SchoolManagement/Repositiory/Interface/IGenericRepositiory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositiory.Interface
+{
+    public interface IGenericRepositiory<T> : IDisposable
+    {
+        Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperty = null);
+
+        T GetById(object id);
+
+        Task<T> GetByIdAsync(object id);
+
+        void Add(T entity);
+
+        Task<T> AddSync(T entity);
+
+        void DeleteById(object id);
+
+        void Delete(T entity);
+
+        Task<T> DeleteAsync(T entity);
+
+        void update(T entity);
+
+        Task<T> UpdateAsync(T entity);
+    }
+}
